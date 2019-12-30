401b29c28a388ef200e23513ba0d34389c344ae9
nik-dev-senpai <55718175+nik-dev-senpai@users.noreply.github.com>
2019-12-30 15:27:45 +0300

    Delete addComment.php

--- .github/workflows/task manager/processing/addComment.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once ($_SERVER['DOCUMENT_ROOT'].'/classes/db.php');
-$db = new db;
-$id = $_POST['id'];
-$db->addComment($id, $_POST['comment']);
-$url = "http://task/editTask.php?id=$id";
-header("Location: $url");
-?>
